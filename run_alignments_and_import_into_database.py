bd5c1214e8592d355e44b04a7c26af86962dd293
jhajagos <risk.limits@gmail.com>
2014-11-30 21:50:00 -0500

    Added postgres option

--- run_alignments_and_import_into_database.py
+++ run_alignments_and_import_into_database.py
@@ -86,7 +86,7 @@ def export_fragments_to_csv(alignment_dict_json_name, alignment_csv_file_name, f
 
     if export_json_file_name is not None:
         add_export_to_json_file(export_json_file_name, table_name, alignment_csv_file_name,
-                                data_types_dict={"fragment": "VarChar(255)", field_name_for_id: "VarChar(255)"},
+                                data_types_dict={"fragment": "VarChar(1023)", field_name_for_id: "VarChar(255)"},
                                 fields_to_index=["fragment", field_name_for_id])
 
 
@@ -95,7 +95,7 @@ def export_sui_info_to_csv(sui_info_json_name, table_export_name, sui_info_csv_f
 
     column_order = ["sui", "umls_string", "fragment", "umls_str_original"]
     column_data_dict = {"sui": "VarChar(255)", "umls_string": "VarChar(1023)",
-                        "umls_str_original": "VarChar(1023)", "fragment": "Varchar(255)"}
+                        "umls_str_original": "VarChar(1023)", "fragment": "Varchar(1023)"}
 
     with open(sui_info_json_name, "r") as f:
         sui_info_dict = json.load(f)
@@ -134,13 +134,55 @@ def add_export_to_json_file(json_file_name, table_name, csv_file_to_load, data_t
 
     export_dict[table_name] = {"data_types_dict": data_types_dict,
                                "fields_to_index": fields_to_index, "csv_file_to_load": csv_file_to_load,
-                               "column_order": header,"line_terminator": line_terminator}
+                               "column_order": header, "line_terminator": line_terminator}
 
     with open(full_path_to_json_file,"w") as fw:
         json.dump(export_dict, fw)
 
 
-def mysql_export_from_json_file(json_file_name):
+def postgres_export_from_json_file(json_file_name, schema="public"):
+    sql_script = ""
+
+    with open(json_file_name, "r") as f:
+        export_tables_dict = json.load(f)
+
+    for table in export_tables_dict:
+        sql_script += 'drop table if exists "%s"."%s";\n\n' % (schema, table)
+
+        table_information = export_tables_dict[table]
+        sql_script += 'create table "%s"."%s" (\n' % (schema, table)
+
+        spacer = "    "
+
+        csv_import_file_name = table_information["csv_file_to_load"]
+        full_path_to_csv_import_file_name = os.path.abspath(csv_import_file_name)
+
+        full_path_to_csv_import_file_name = '/'.join(full_path_to_csv_import_file_name.split('\\'))
+        columns = table_information["column_order"]
+        data_types_dict = table_information["data_types_dict"]
+
+        i = 0
+        for column in columns:
+            sql_script += """%s "%s" %s,\n""" % (spacer, column, data_types_dict[column])
+
+        sql_script = sql_script[:-2] + ");\n\n"
+
+        sql_script += '''/* copy "%s"."%s" from '%s' WITH DELIMITER ',' CSV HEADER; */ \n''' % (schema, table, full_path_to_csv_import_file_name)
+        sql_script += '''/* \\copy "%s"."%s" from '%s' WITH DELIMITER ',' CSV HEADER; */ \n\n''' % (schema, table, full_path_to_csv_import_file_name)
+
+        indexes_to_create = table_information["fields_to_index"]
+        sql_script += ""
+
+        i = 0
+        for index_to_create in indexes_to_create:
+            index_name = "idx_" + table[0] + str(random.randint(0, 999)) + str(i)
+            sql_script += """create index %s on "%s"."%s"("%s");\n""" % (index_name, schema, table, index_to_create)
+            i += 1
+
+    return sql_script
+
+
+def mysql_export_from_json_file(json_file_name, local="LOCAL"):
     re_extract_field_length = re.compile(r'\(([0-9]+)\)')
     sql_script = ""
 
@@ -164,12 +206,12 @@ def mysql_export_from_json_file(json_file_name):
         line_terminator = table_information["line_terminator"]
         i = 0
         for column in columns:
-            sql_script += "%s %s %s,\n" % (spacer, column, data_types_dict[column] )
+            sql_script += "%s `%s` %s,\n" % (spacer, column, data_types_dict[column] )
 
         sql_script += "%sprimary key(id));\n\n" % spacer
 
         sql_script += "/* Load data into CSV file */\n\n"
-        sql_script += "LOAD DATA INFILE '%s' INTO TABLE `%s` \n" % (full_path_to_csv_import_file_name, table)
+        sql_script += "LOAD DATA %s  INFILE '%s' INTO TABLE `%s` \n" % (local, full_path_to_csv_import_file_name, table)
         sql_script += """FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\0'
         LINES TERMINATED BY '%s' IGNORE 1 LINES\n""" % line_terminator
 
@@ -204,7 +246,7 @@ def mysql_export_from_json_file(json_file_name):
         i = 0
         for index_to_create in indexes_to_create:
             index_name = "idx_" + table[0] + str(random.randint(0,999)) + str(i)
-            sql_script += "create index %s on %s(%s);\n" % (index_name, table, index_to_create)
+            sql_script += "create index %s on `%s`(`%s`);\n" % (index_name, table, index_to_create)
             i += 1
 
     return sql_script
@@ -250,8 +292,21 @@ def main(json_config_file, generate_fragments=True):
                                        additional_constant_field=configuration["additional constant field"],
                                        table_export_name=alignment_table_name,
                                        additional_list_of_fields_to_index=configuration["additional fields to index"])
+    if "database_type" in configuration:
+        database_type = configuration["database_type"]
+    else:
+        database_type = "mysql"
 
-    sql_script = mysql_export_from_json_file(json_file_dict_name)
+    if database_type == "mysql":
+        sql_script = mysql_export_from_json_file(json_file_dict_name)
+    elif database_type == "postgres":
+        if "schema" in configuration:
+            schema = configuration["schema"]
+        else:
+            schema = "public"
+
+        sql_script = postgres_export_from_json_file(json_file_dict_name, schema)
+
     sql_import_file = os.path.join(full_path_directory, configuration_to_run["class"] + "_load" + ".sql")
 
     with open(sql_import_file, "w") as f:
@@ -273,7 +328,7 @@ if __name__ == "__main__":
                                                   }]}
 
         with open("example.json", "w") as fw:
-            json.dump(example_config, fw)
+            json.dump(example_config, fw, indent=4, separators=(',', ': '))
 
         main("example.json")
 
